b7bd59e6afcc799e66ab2b1144bb89705bd9bad2
Wakake <109547016+Wakake@users.noreply.github.com>
2024-04-30 15:15:31 +0530

    Add files via upload

---
+++ AddTwoNumbers.java
@@ -0,0 +1,8 @@
+public class AdditionProgram {
+    public static void main(String[] args) {
+        int num1 = 6;
+        int num2 = 100;
+        int sum = num1 + num2;
+        System.out.println("Sum: " + sum);
+    }
+}
